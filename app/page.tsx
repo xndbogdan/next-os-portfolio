f399da0afda8b06dea05f74c2d7025de11c4f89c
Bogdan Mihai <27998462+xndbogdan@users.noreply.github.com>
2024-06-18 08:35:21 +0300

    revert wallpaper

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const shuffledTracklist: Tracklist = tracklist.sort((a, b) => 0.5 - Math.random(
 
 export default function Home() {
   return (
-    <div className="select-none cursor-default overflow-hidden bg-tile-4 text-black">
+    <div className="select-none cursor-default overflow-hidden bg-tile-3 text-black">
       <Desktop tracklist={shuffledTracklist} />
     </div>
   );
